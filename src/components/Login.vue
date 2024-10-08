39296b0df05c68ac72e6f1826d8c0a2de351bc41
Sergejs Makarovs <sergejs.makarovs@magebit.com>
2024-10-08 10:50:49 +0300

    Mainpage update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,58 +1,57 @@
 <template>
-    <div class="login-form">
-      <h2 class="modal-title">Login</h2>
-      <form @submit.prevent="submitLogin">
-        <div class="form-group">
-          <label for="email">E-pasts</label>
-          <input type="email" v-model="email" required class="input-box" />
-        </div>
-        <div class="form-group">
-          <label for="password">Parole</label>
-          <input type="password" v-model="password" required class="input-box" />
-        </div>
-        <button type="submit" class="btn">Ieiet</button>
-      </form>
-    </div>
-  </template>
-  
+  <div class="login-form">
+    <h2 class="modal-title">Login</h2>
+    <form @submit.prevent="submitLogin">
+      <div class="form-group">
+        <label for="email">E-pasts</label>
+        <input v-model="email" class="input-box" required type="email">
+      </div>
+      <div class="form-group">
+        <label for="password">Parole</label>
+        <input v-model="password" class="input-box" required type="password">
+      </div>
+      <button class="btn" type="submit">Ieiet</button>
+    </form>
+  </div>
+</template>
+
   <script>
   export default {
-    data() {
+    data () {
       return {
         email: '',
-        password: ''
-      };
+        password: '',
+      }
     },
     methods: {
-      submitLogin() {
-       
-        console.log('Logging in with', this.email, this.password);
-      }
-    }
-  };
+      submitLogin () {
+        console.log('Logging in with', this.email, this.password)
+      },
+    },
+  }
   </script>
-  
+
   <style scoped>
   .login-form {
     display: flex;
     flex-direction: column;
     gap: 1rem;
   }
-  
+
   .form-group {
     display: flex;
     flex-direction: column;
   }
-  
+
   .btn {
     background-color: #007bff;
     color: white;
     padding: 10px;
     border: none;
     cursor: pointer;
-    
+
   }
-  
+
   .btn:hover {
     background-color: #0056b3;
   }
---
+++ src/components/ProductGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-center m-[100px] flex-col p-4">
+    <div class="p-6 border-2 rounded-md">
+      there will be filters and search
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 pt-2 box-border">
+      <div v-for="(item, index) in items" :key="index" class="border rounded min-w-[200px] box-border">
+        <div class="border-b  w-full h-[196px]">Image</div>
+        <div class="p-2">
+          <p>Some Flower</p>
+          <p>$100</p>
+          <button class="bg-blue-500 text-white box-border py-2 px-4 rounded w-full">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: Array.from({ length: 10 }),
+      }
+    },
+  }
+</script>
