ad07e0831f84a7527001f73f98b77b48f08dbe76
Sergejs Makarovs <sergejs.makarovs@magebit.com>
2024-10-23 12:52:15 +0300

    connect product page to db finished

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
-    <!-- <div v-if="loading" class="text-center">
+    <div v-if="loading" class="text-center">
       <p>Loading product details...</p>
     </div>
     <div v-else class="flex flex-col lg:flex-row items-start lg:space-x-8 space-y-8 lg:space-y-0">
 
       <div class="w-full lg:w-1/2">
-        <img alt="Product Image" class="w-full h-auto object-cover rounded-lg" :src="product.mainImage">
+        <img alt="Product Image" class="w-full h-auto object-cover rounded-lg" :src="data.options[0].images[0]">
         <div class="mt-4 flex overflow-x-auto space-x-2">
           <img
-            v-for="(image, index) in product.gallery"
+            v-for="(image, index) in data.options[0].images"
             :key="index"
             alt="Gallery Image"
             class="w-20 h-20 object-cover flex-shrink-0 rounded-lg border border-gray-300 transition-transform transform hover:scale-105"
@@ -19,11 +19,11 @@
       </div>
 
       <div class="w-full lg:w-1/2">
-        <h1 class="text-3xl font-bold text-gray-900">{{ product.title }}</h1>
-        <p class="mt-2 text-gray-600">{{ product.Description }}</p>
+        <h1 class="text-3xl font-bold text-gray-900">{{ data.name }}</h1>
+        <p class="mt-2 text-gray-600">{{ data.options[0].description }}</p>
 
         <div class="mt-4">
-          <span class="text-2xl font-semibold text-gray-900">${{ product.price }}</span>
+          <span class="text-2xl font-semibold text-gray-900">${{ calculateFinalPrice(data) }}</span>
         </div>
 
         <div class="mt-4 flex items-center">
@@ -31,6 +31,7 @@
           <input
             v-model="quantity"
             class="w-20 border border-gray-300 rounded-lg px-2"
+            :max="data.options[0].quantity"
             min="1"
             type="number"
           >
@@ -51,7 +52,7 @@
           </button>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -59,16 +60,14 @@
   export default {
     data () {
       return {
-        loading: false,
+        loading: true,
         quantity: 1, // Initialize quantity
         data: [],
       }
     },
     mounted () {
-      this.loading = false
       this.getProduct()
     },
-
     methods: {
       buyNow () {
         alert(`Redirecting to checkout for ${this.quantity} items...`)
@@ -77,6 +76,8 @@
         alert(`Added ${this.quantity} items to cart!`)
       },
       async getProduct () {
+        this.loading = true
+
         try {
           const response = await fetch(`https://ziedu-veikals.vercel.app/product/${this.$route.params.id}`, {
             method: 'GET',
@@ -95,8 +96,14 @@
         } catch (error) {
           console.error("Couldn't get featured products", error)
         } finally {
-          this.Loading = false
+          this.loading = false
         }
+      },
+      calculateFinalPrice (product) {
+        const price = product.options[0].price
+        const discount = product.discount
+
+        return discount ? (price - price * (discount / 100)).toFixed(2) : price.toFixed(2)
       },
     },
   }
